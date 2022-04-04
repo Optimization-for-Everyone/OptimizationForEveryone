01f4467ac43475b29552a9026913edadf84e68cb
Yigithan Guven <demirejderha@gmail.com>
2022-04-04 17:36:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,106 @@
+#------------------------------------------------------
+# ---------------------- main.py -----------------------
+# ------------------------------------------------------
+from PyQt5.QtWidgets import*
+from PyQt5.uic import loadUi
+import numpy
+from HHO import HHO, Levy
+from SMA import BaseSMA, OriginalSMA
+from numpy import sum, pi, exp, sqrt, cos
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+
+import numpy as np
+import random
+
+#Functions
+def func_sum(solution):
+    return sum(solution ** 2)
+def func_ackley(solution):
+    a, b, c = 20, 0.2, 2 * pi
+    d = len(solution)
+    sum_1 = -a * exp(-b * sqrt(sum(solution ** 2) / d))
+    sum_2 = exp(sum(cos(c * solution)) / d)
+    return sum_1 - sum_2 + a + exp(1)
+#Select wich function used;
+def selectFunction(cbIndex):
+        if cbIndex==0 :
+            return func_sum
+        elif cbIndex==1 :
+            return func_ackley
+#Optimization Algorthms
+def Run_HHO(functionIndex):
+        #lb=TextBox_lb.text
+        lb = [-32768]
+        ub = [32768]
+        obj_func=selectFunction(functionIndex)  
+        dim = 30
+        SearchAgents_no = 1000
+        Max_iter = 50
+        solution = HHO(obj_func, lb, ub, dim, SearchAgents_no, Max_iter)
+        sol = np.array(solution.result)
+        return sol
+def Run_SMA(functionIndex):
+        lb = [-100]
+        ub = [100]
+        problem_size = 100       
+        obj_func=selectFunction(functionIndex)
+        verbose = True
+        epoch = 50
+        pop_size = 50
+        md1 = BaseSMA(obj_func, lb, ub, problem_size, verbose, epoch, pop_size)
+        best_pos1, best_fit1, list_loss1, sol1 = md1.train()
+        sol=numpy.array(sol1)
+        # return : the global best solution, the fitness of global best solution and the loss of training process in each epoch/iteration
+        print(md1.solution[0])
+        print(md1.solution[1])
+        print(md1.loss_train)
+        return sol    
+
+class MatplotlibWidget(QMainWindow):
+    
+    def __init__(self):
+        
+        QMainWindow.__init__(self)
+
+        loadUi("qt_designer.ui",self)
+
+        self.setWindowTitle("Optimization Algorthms")
+
+        self.pushButton.clicked.connect(self.Plot)
+        
+        #Add items to functions combo Box
+        self.functionComboBox.addItem('X^2')
+        self.functionComboBox.addItem('Ackley Function')
+        #Add items to optimization combo Box
+        self.optimizationComboBox.addItem('HHO')
+        self.optimizationComboBox.addItem('SMA')
+        
+        self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
+
+    
+
+        #self.MplWidget.canvas.axes.clear()
+        #self.MplWidget.canvas.axes.plot(t, cosinus_signal)
+        #self.MplWidget.canvas.axes.plot(t, sinus_signal)
+        #self.MplWidget.canvas.axes.legend(('cosinus', 'sinus'),loc='upper right')
+        #self.MplWidget.canvas.axes.set_title('Cosinus - Sinus Signal')
+        #self.MplWidget.canvas.draw()
+        
+         
+    def Plot(self,sol):
+        if self.optimizationComboBox.currentIndex()==0 :
+            sol=Run_HHO(self.functionComboBox.currentIndex())
+        elif self.optimizationComboBox.currentIndex()==1 :
+            sol=Run_SMA(self.functionComboBox.currentIndex())
+        self.MplWidget.canvas.axes.clear()
+        self.MplWidget.canvas.axes.plot(sol[1:,1:])
+        self.MplWidget.canvas.axes.legend(('Iteration', 'Best fitness'),loc='upper right')
+        self.MplWidget.canvas.axes.set_title('Convergence curve')
+        self.MplWidget.canvas.draw()
+
+
+
+app = QApplication([])
+window = MatplotlibWidget()
+window.show()
+app.exec_()
---
+++ test_HHO.py
@@ -0,0 +1,51 @@
+import numpy
+from HHO import HHO, Levy
+from numpy import sum, pi, exp, sqrt, cos
+import matplotlib.pyplot as plt 
+
+
+## You can create whatever function you want here
+def func_sum(solution):
+    return sum(solution ** 2)
+
+
+def func_ackley(solution):
+    a, b, c = 20, 0.2, 2 * pi
+    d = len(solution)
+    sum_1 = -a * exp(-b * sqrt(sum(solution ** 2) / d))
+    sum_2 = exp(sum(cos(c * solution)) / d)
+    return sum_1 - sum_2 + a + exp(1)
+
+
+lb = [-32768]
+ub = [32768]
+
+obj_func = func_sum
+dim = 30
+SearchAgents_no = 1000
+Max_iter = 50
+
+solution = HHO(obj_func, lb, ub, dim, SearchAgents_no, Max_iter)
+sol = numpy.array(solution.result)
+
+
+
+""""
+lb = [-100]
+ub = [100]
+
+obj_func = func_sum
+dim = 30
+SearchAgents_no = 50
+Max_iter = 10
+
+solution = HHO(obj_func, lb, ub, dim, SearchAgents_no, Max_iter)
+sol = numpy.array(solution.result)
+"""
+
+
+plt.xlabel ('Iteration', fontsize = 12)
+plt.ylabel ('Best fitness obtained so far', fontsize = 12)
+plt.title ("Convergence curve", fontsize = 12)
+plt.plot(sol[1:,1:]) 
+plt.show()
