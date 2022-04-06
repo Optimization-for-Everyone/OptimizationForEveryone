32816473c3c49f89c4bb0c57b77a751f80f6a10b
Yigithan Guven <demirejderha@gmail.com>
2022-04-06 21:40:06 +0300

    MulipleRunsAdded

--- main.py
+++ main.py
@@ -31,36 +31,18 @@ class MatplotlibWidget(QMainWindow):
         QMainWindow.__init__(self)
 
         loadUi("qt_designer.ui",self)
+        AddItemsToComboBox(self)
         
 
         self.setWindowTitle("Optimization Algorthms")
-
+        #Set button functions
+        
         self.pushButton.clicked.connect(self.Plot)
         self.inputButton.clicked.connect(self.InputButton)
         self.infoButton.clicked.connect(self.InfoButton)
+        self.runMultipleButton.clicked.connect(self.PlotMultiple)
         
-        #Add items to functions combo Box
-        self.functionComboBox.addItem('ackley')
-        self.functionComboBox.addItem('dixonprice')
-        self.functionComboBox.addItem('griewank')
-        self.functionComboBox.addItem('michalewicz')
-        self.functionComboBox.addItem('perm')
-        self.functionComboBox.addItem('powell')
-        self.functionComboBox.addItem('powersum')
-        self.functionComboBox.addItem('rastrigin')
-        self.functionComboBox.addItem('rosenbrock')
-        self.functionComboBox.addItem('schwefel')
-        self.functionComboBox.addItem('sphere')
-        self.functionComboBox.addItem('sum2')
-        self.functionComboBox.addItem('trid')
-        self.functionComboBox.addItem('zakharov')
-        self.functionComboBox.addItem('ellipse')
-        self.functionComboBox.addItem('nesterov')
-        self.functionComboBox.addItem('saddle')
-        #Add items to optimization combo Box
-        self.optimizationComboBox.addItem('HHO')
-        self.optimizationComboBox.addItem('SMA')
-        self.optimizationComboBox.addItem('GA')
+       
         
         self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
 
@@ -83,6 +65,17 @@ class MatplotlibWidget(QMainWindow):
         self.MplWidget.canvas.axes.plot(sol[1:,1:])
         self.MplWidget.canvas.axes.legend(('Iteration', 'Best fitness'),loc='upper right')
         self.MplWidget.canvas.axes.set_title('Convergence curve')
+        self.MplWidget.canvas.draw()
+    def PlotMultiple(self,sol):
+        sol=Run_Optimization.Run_HHO(self.functionComboBox.currentIndex(),int(self.MaxIter),int(self.dimension),int(self.searchAgentsNo),int(self.lb[0]),int(self.ub[0]))
+        sol2=Run_Optimization.Run_SMA(self.functionComboBox.currentIndex(),int(self.problem_size),self.verbose,int(self.epoch),int(self.pop_size),int(self.smalb[0]),int(self.smaub[0]))
+        sol3=Run_Optimization.Run_GA(self.functionComboBox.currentIndex(),int(self.MaxIter),int(self.dimension),int(self.searchAgentsNo),int(self.lb[0]),int(self.ub[0]))
+        self.MplWidget.canvas.axes.clear()
+        self.MplWidget.canvas.axes.plot(sol[1:,1:])
+        self.MplWidget.canvas.axes.plot(sol2[1:,1:])
+        self.MplWidget.canvas.axes.plot(sol3[1:,1:])
+        self.MplWidget.canvas.axes.legend(('HHO', 'SMA','GA'),loc='upper right')
+        self.MplWidget.canvas.axes.set_title('Convergence curve') 
         self.MplWidget.canvas.draw()
     def InputButton(self):
         if self.optimizationComboBox.currentIndex()==1:
@@ -117,7 +110,31 @@ class MatplotlibWidget(QMainWindow):
     #=================
     #Info Ok Buttons
     def AckleyInfoOkButton(self):        
-        self.window.close()   
+        self.window.close()
+    
+def AddItemsToComboBox(self):
+         #Add items to functions combo Box
+        self.functionComboBox.addItem('ackley')
+        self.functionComboBox.addItem('dixonprice')
+        self.functionComboBox.addItem('griewank')
+        self.functionComboBox.addItem('michalewicz')
+        self.functionComboBox.addItem('perm')
+        self.functionComboBox.addItem('powell')
+        self.functionComboBox.addItem('powersum')
+        self.functionComboBox.addItem('rastrigin')
+        self.functionComboBox.addItem('rosenbrock')
+        self.functionComboBox.addItem('schwefel')
+        self.functionComboBox.addItem('sphere')
+        self.functionComboBox.addItem('sum2')
+        self.functionComboBox.addItem('trid')
+        self.functionComboBox.addItem('zakharov')
+        self.functionComboBox.addItem('ellipse')
+        self.functionComboBox.addItem('nesterov')
+        self.functionComboBox.addItem('saddle')
+        #Add items to optimization combo Box
+        self.optimizationComboBox.addItem('HHO')
+        self.optimizationComboBox.addItem('SMA')
+        self.optimizationComboBox.addItem('GA')
 
   
 
