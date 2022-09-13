b5d0892b7bd54f46f6bf33e2c448daf2080d8772
Mustafa Harun Türkmenoğlu <75381086+mharunturkmenoglu@users.noreply.github.com>
2022-09-13 19:07:25 +0300

    feat: add BAT2 and FFA2

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ from write_operations import WriteOperations
 from enumFunctions import Functions
 from enumOptimizations import Optimizations
 from multipleRun import MultipleRun
+from algorithms.BAT2 import BatAlgorithm
+from algorithms.FFA2 import FireflyAlgorithm
+import functions
 
 
 class Test:
@@ -12,9 +15,9 @@ class Test:
         self._lb = -32768
         self._ub = 32768
         self.dim = 30
-        self.problem_size = 30
-        self.searchAgents_no = 100
-        self.maxiter = 10
+        #self.problem_size = 30
+        self.searchAgents_no = 100 # pop size
+        self.maxiter = 25
         self.verbose = True
         self.params = self.functionIndex, self.maxiter, self.dim, self.searchAgents_no, self._lb, self._ub
         self.sma = self.functionIndex, self.dim, self.verbose, self.maxiter, self.searchAgents_no, self._lb, self._ub 
@@ -24,12 +27,49 @@ class Test:
         WriteOperations(opt.name,self.functionIndex.name,sol).write()
         return sol
     
-    def MultipleRun(self):
+    def MultipleRun(self, opt = Optimizations.FFA):
         MultipleRun(self.opt, self.params, self.sma, number = 5)
 
+    def RunBAT2(self):
+        obj_func = functions.selectFunction(Functions.ackley)
+
+        # D = Dimension, NP = Population size, N_Gen = Number of Generation
+        bat = BatAlgorithm(obj_func,Lower=-32768,Upper=32768,D=30,NP=1000,N_Gen = 200) 
+
+        bat.move_bat()
+    
+    def RunFFA2(self):
+        obj_func = functions.selectFunction(Functions.ackley)
+
+        ffa = FireflyAlgorithm()
+
+        ffa.run(obj_func, dim = 30, lb=-32768, ub=32768, max_evals=200)
+
+
+
+
 def main():
+
     test = Test()
-    sol = test.Run()
+
+    #-----------MULTIRUN TESTING START-----------#
+
+    #test.MultipleRun(Optimizations.FFA)
+
+    #-----------MULTIRUN TESTING END-------------#
+
+    #-----------BAT 2 TESTING START-----------#
+
+    #test.RunBAT2()
+
+    #-----------BAT 2 TESTING END-----------#
+
+    #-----------FFA 2 TESTING START-----------#
+
+    test.RunFFA2()
+
+    #-----------FFA 2 TESTING END-------------#
+    
 
     
 
